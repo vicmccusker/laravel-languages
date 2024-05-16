eac314ddfc8b64628926585a8d926174456d65f0
Victoria McCusker <vrmm@hotmail.co.uk>
2024-05-16 12:16:26 +0100

    adding difficulty image

--- database/migrations/2024_03_21_155637_create_difficulties_table.php
+++ database/migrations/2024_03_21_155637_create_difficulties_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('difficulties', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('difficulty_image');
             $table->timestamps();
         });
     }
--- database/seeders/DifficultySeeder.php
+++ database/seeders/DifficultySeeder.php
@@ -15,12 +15,15 @@ class DifficultySeeder extends Seeder
         $toInsert = [
             [
                 'name' => 'Easy',
+                'difficulty_image' => 'https://jmp.sh/s/qWOwgfgPfr9G9ZsEUTqT',
             ],
             [
                 'name' => 'Mid',
+                'difficulty_image' => 'https://jmp.sh/s/YTf4JE3pXkcFRPUpyqZ1',
             ],
             [
                 'name' => 'Hard',
+                'difficulty_image' => 'https://svgshare.com/s/169c'
             ],
 
         ];
